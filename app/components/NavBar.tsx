5653faa696e8ac4046d8176cc615bd085d253949
NateAra <nate.arafayne@appliedtechnology.se>
2024-09-09 14:35:34 +0200

    style: remove boder

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,7 +5,7 @@ import { ModeToggle } from "./ModeToggle";
 export const NavBar = () => {
 
   return (
-    <header className="w-full border-2">
+    <header className="w-full">
       <nav className="flex items-center justify-between px-4 py-5">
         <Link href={'/'} className="font-bold text-5xl">
           Nate<span className="text-primary">Blog</span>
