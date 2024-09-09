6d8eae14b3f03d5f7fa3bd18cec9389d2aeb8eaf
NateAra <nate.arafayne@appliedtechnology.se>
2024-09-09 13:51:31 +0200

    feat: create navBar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,13 +2,12 @@ import Link from "next/link";
 import React from "react";
 
 export const NavBar = () => {
-  const navItems = ["Item1", "Item2", "Item3"];
 
   return (
     <header className="w-full border-2">
       <nav className="flex items-center justify-between px-4 py-5">
         <Link href={'/'} className="font-bold text-5xl">
-          Nate<span className="text-indigo-600">Blog</span>
+          Nate<span className="text-primary">Blog</span>
         </Link>
       </nav>
     </header>
